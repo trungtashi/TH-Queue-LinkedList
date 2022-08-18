d9a8f4aa2ea6b8e90c0444cef2a067298b66d3e8
trungtashi99 <trungtashi@gmai.com>
2022-08-18 16:14:47 +0700

    Thực hành Queue LinkedList

---
+++ src/LinkedListQueueClient.java
@@ -0,0 +1,12 @@
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    Object next;
+    public int key;
+    public Node text;
+
+    public  Node(int key){
+        this.key = key;
+        this.next = null;
+    }
+
+}
